5d0e79be6e7425cd90fcde2b7ee98e65f2b7f1ea
Astra <abhaypratapsinghwd@gmail.com>
2024-05-20 11:42:10 +0530

    Added login and regsiter routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Employee API is running" });
 });
 
-// app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
