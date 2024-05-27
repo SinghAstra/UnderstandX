ded0da45c2de1ef7a614194a16842fe31d43012b
Astra <abhaypratapsinghwd@gmail.com>
2024-05-27 13:17:39 +0530

    Ensured Email and Phone Number Uniqueness

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -2,21 +2,37 @@ const { validationResult } = require("express-validator");
 const Employee = require("../models/Employee");
 
 const createEmployee = async (req, res) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-      jobTitle,
-      team,
-      dateOfHire,
-      profilePicture,
-    } = req.body;
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    jobTitle,
+    team,
+    dateOfHire,
+    profilePicture,
+  } = req.body;
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ success: false, errors: errors.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success: false, errors: errors.array() });
+  }
+
+  try {
+    // Check if the email or phone number already exists
+    const existingEmployee = await Employee.findOne({
+      $or: [{ email }, { phoneNumber }],
+    });
+
+    if (existingEmployee) {
+      if (existingEmployee.email === email) {
+        return res.status(400).json({ message: "Email already registered!" });
+      }
+      if (existingEmployee.phoneNumber === phoneNumber) {
+        return res
+          .status(400)
+          .json({ message: "Phone number already registered!" });
+      }
     }
 
     const newEmployee = new Employee({
