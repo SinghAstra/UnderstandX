839f32e41fe1dbb7cd876acf79adc2978f03bbc0
Astra <abhaypratapsinghwd@gmail.com>
2024-10-30 08:22:28 +0530

    Updated Config

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "SchemaX",
+  name: "SchemaVizX",
   description:
     "Instantly visualize database schemas and generate AI-ready prompts for faster, smarter codebase understanding",
-  url: "https://schemax.vercel.app",
+  url: "https://schemavizx.vercel.app",
   links: {
     twitter: "https://x.com/XSinghAstra",
-    github: "https://github.com/SinghAstra/SchemaX",
+    github: "https://github.com/SinghAstra/SchemaVizX",
     linkedIn: "https://www.linkedin.com/in/singhastra",
   },
 };
