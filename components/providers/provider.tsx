a5cbe198f88fd22b345bf34a43d251f5d7d628d1
Astra <abhaypratapsinghwd@gmail.com>
2025-01-02 17:35:20 +0530

    Deployment Bug Fix

--- components/providers/provider.tsx
+++ components/providers/provider.tsx
@@ -1,18 +1,28 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import React from "react";
+import React, { Suspense } from "react";
 import { TooltipProvider } from "../ui/tooltip";
 
 interface Props {
   children: React.ReactNode;
 }
 
+const LoadingFallback = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div>
+    </div>
+  );
+};
+
 const Providers = ({ children }: Props) => {
   return (
-    <SessionProvider>
-      <TooltipProvider>{children}</TooltipProvider>
-    </SessionProvider>
+    <Suspense fallback={<LoadingFallback />}>
+      <SessionProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </SessionProvider>
+    </Suspense>
   );
 };
 
