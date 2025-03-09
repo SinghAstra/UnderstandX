9d1f0b595a37720479aab5e16dcf31dc39b8e1ec
Astra <abhaypratapsinghwd@gmail.com>
2025-03-09 09:49:22 +0530

    Added Footer & Navbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,16 +1,23 @@
-import { Navbar } from "@/components/home/navbar";
+import Footer from "@/components/navigation/footer";
+import Navbar from "@/components/navigation/navbar";
+import React from "react";
 
-interface HomeLayoutProps {
+interface Props {
   children: React.ReactNode;
 }
 
-export default async function HomeLayout({ children }: HomeLayoutProps) {
+const MarketingLayout = ({ children }: Props) => {
   return (
     <>
-      <div className="flex flex-col ">
-        <Navbar />
-        {children}
-      </div>
+      <div
+        id="home"
+        className="absolute inset-0 dark:bg-[linear-gradient(to_right,#e2e8f0_1px,transparent_1px),linear-gradient(to_bottom,#e2e8f0_1px,transparent_1px)] bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full"
+      />
+      <Navbar />
+      <main className="mt-20 mx-auto w-full z-0 relative">{children}</main>
+      <Footer />
     </>
   );
-}
+};
+
+export default MarketingLayout;
