affe4a5cad4045c0b8c3d2b6a6db809dc1decebd
Astra <abhaypratapsinghwd@gmail.com>
2025-03-11 04:47:00 +0530

    Added the Lamp

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,11 +5,11 @@ import MaxWidthWrapper from "@/components/global/max-width-wrapper";
 import { BentoCard, BentoGrid, Steps } from "@/components/ui/bento-grid";
 import { BorderBeam } from "@/components/ui/border-beam";
 import { Button } from "@/components/ui/button";
-
-import { LampDemo } from "@/components/ui/lamp";
+import { LampContainer } from "@/components/ui/lamp";
 import MagicBadge from "@/components/ui/magic-badge";
 import { useToast } from "@/hooks/use-toast";
 import { ArrowRightIcon } from "lucide-react";
+import { motion } from "motion/react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -126,9 +126,16 @@ const HomePage = () => {
 
       {/* CTA Section */}
       <MaxWidthWrapper className="mt-20 max-w-[100vw] overflow-x-hidden scrollbar-hide">
-        <AnimationContainer delay={0.3}>
-          <LampDemo />
-        </AnimationContainer>
+        <LampContainer>
+          <motion.h1
+            initial={{ opacity: 0.5, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
+            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+          >
+            Build lamps <br /> the right way
+          </motion.h1>
+        </LampContainer>
       </MaxWidthWrapper>
     </div>
   );
