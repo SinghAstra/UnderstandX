ace24faccbc997fd05d4909359d78b291a0e87ca
Astra <abhaypratapsinghwd@gmail.com>
2025-04-09 11:01:23 +0530

    pusher keys error handling

--- lib/pusher/client.ts
+++ lib/pusher/client.ts
@@ -1,7 +1,16 @@
 import Pusher from "pusher-js";
 
-const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+const NEXT_PUBLIC_PUSHER_APP_KEY = process.env.NEXT_PUBLIC_PUSHER_APP_KEY;
+const NEXT_PUBLIC_PUSHER_CLUSTER = process.env.NEXT_PUBLIC_PUSHER_CLUSTER;
+
+if (!NEXT_PUBLIC_PUSHER_APP_KEY || !NEXT_PUBLIC_PUSHER_CLUSTER) {
+  throw new Error(
+    "NEXT_PUBLIC_PUSHER_APP_KEY and NEXT_PUBLIC_PUSHER_CLUSTER must be provided."
+  );
+}
+
+const pusherClient = new Pusher(NEXT_PUBLIC_PUSHER_APP_KEY, {
+  cluster: NEXT_PUBLIC_PUSHER_CLUSTER,
 });
 
 export default pusherClient;
--- lib/pusher/server.ts
+++ lib/pusher/server.ts
@@ -1,10 +1,26 @@
 import Pusher from "pusher";
 
+const PUSHER_APP_ID = process.env.PUSHER_APP_ID;
+const NEXT_PUBLIC_PUSHER_APP_KEY = process.env.NEXT_PUBLIC_PUSHER_APP_KEY;
+const PUSHER_SECRET = process.env.PUSHER_SECRET;
+const NEXT_PUBLIC_PUSHER_CLUSTER = process.env.NEXT_PUBLIC_PUSHER_CLUSTER;
+
+if (
+  !PUSHER_APP_ID ||
+  !NEXT_PUBLIC_PUSHER_APP_KEY ||
+  !PUSHER_SECRET ||
+  !NEXT_PUBLIC_PUSHER_CLUSTER
+) {
+  throw new Error(
+    "PUSHER_APP_ID, NEXT_PUBLIC_PUSHER_APP_KEY, PUSHER_SECRET and NEXT_PUBLIC_PUSHER_CLUSTER must be provided."
+  );
+}
+
 const pusherServer = new Pusher({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  appId: PUSHER_APP_ID,
+  key: NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: PUSHER_SECRET,
+  cluster: NEXT_PUBLIC_PUSHER_CLUSTER,
   useTLS: true,
 });
 
