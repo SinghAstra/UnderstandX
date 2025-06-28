552343b9afc34ca261ee7bfe167c601eb3e34898
Astra <abhaypratapsinghwd@gmail.com>
2025-06-28 18:27:44 +0530

    Updated Not Found

--- components/home/navbar.tsx
+++ components/home/navbar.tsx
@@ -1,10 +1,7 @@
 import { siteConfig } from "@/config/site";
-import { cn } from "@/lib/utils";
 import { User } from "next-auth";
 import Link from "next/link";
 import React from "react";
-import FadeIn from "../global/fade-in";
-import MaxWidthWrapper from "../global/max-width-wrapper";
 import { AvatarMenu } from "../ui/avatar-menu";
 import SignIn from "../ui/sign-in";
 
@@ -14,27 +11,17 @@ interface NavbarProps {
 
 const Navbar = ({ user }: NavbarProps) => {
   return (
-    <header
-      className={cn(
-        "fixed top-0 inset-x-0 h-14 z-[99] border-b border-dashed backdrop-blur-lg"
-      )}
-    >
-      <FadeIn delay={0.1} className="size-full">
-        <MaxWidthWrapper className="flex items-center justify-between">
-          <div className="flex items-center space-x-12">
-            <Link href="/">
-              <span className="text-3xl font-normal tracking-wider">
-                {siteConfig.name}
-              </span>
-            </Link>
-          </div>
+    <div className="sticky top-0 inset-x-0 h-14 z-[99] backdrop-blur-lg">
+      <div className="flex items-center justify-between px-3 md:px-4 lg:px-5 py-2 md:py-3">
+        <Link href="/">
+          <span className="text-3xl font-normal tracking-wider">
+            {siteConfig.name}
+          </span>
+        </Link>
 
-          <div className="hidden lg:flex items-center">
-            {user ? <AvatarMenu user={user} /> : <SignIn />}
-          </div>
-        </MaxWidthWrapper>
-      </FadeIn>
-    </header>
+        {user ? <AvatarMenu user={user} /> : <SignIn />}
+      </div>
+    </div>
   );
 };
 
