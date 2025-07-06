0f66db384c29751fbcc082d5bb7f28014077ac8b
Astra <abhaypratapsinghwd@gmail.com>
2025-07-06 11:56:15 +0530

    bug fix

---
+++ interfaces/github.ts
@@ -0,0 +1,25 @@
+import { Directory, File, Repository } from "@prisma/client";
+
+export interface GitHubContent {
+  name: string;
+  type: "file" | "dir";
+  path: string;
+  content?: string;
+}
+
+export interface DirectoryWithRelations extends Directory {
+  children: DirectoryWithRelations[];
+  files: File[];
+}
+
+export interface RepositoryWithRelationsAndOverview extends Repository {
+  directories: DirectoryWithRelations[];
+  rootFiles: File[];
+  files: File[];
+}
+
+export interface FileMetaData {
+  id: string;
+  name: string;
+  path: string;
+}
